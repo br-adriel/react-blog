3d080a8625b074a70bc7b61f2a98e93fcce1fdb3
Adriel <adriel.fsantos@outlook.com>
2023-03-12 19:26:32 -0300

    Inicializa projeto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import GlobalStyle from './components/GlobalStyle';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Olá mundo</h1>
+    </>
+  );
+}
+
+export default App;

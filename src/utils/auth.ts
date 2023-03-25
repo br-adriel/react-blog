4c50c6fc48b2eb9ad68d93ddc33c0477b6cbafc0
Adriel <adriel.fsantos@outlook.com>
2023-03-25 13:23:05 -0300

    Implementa funções storeUserProfile e getStoredUserProfile

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,3 +1,10 @@
+import { UserProfile } from '../types/userSlice';
+
+export function storeUserProfile(profile: UserProfile) {
+  const stringProfile = JSON.stringify(profile);
+  localStorage.setItem('profile', stringProfile);
+}
+
 export function storeRefeshToken(refreshToken: string) {
   localStorage.setItem('refreshToken', refreshToken);
 }
@@ -12,4 +19,13 @@ export function getStoredRefeshToken() {
 
 export function getStoredToken() {
   localStorage.getItem('token');
+}
+
+export function getStoredUserProfile(): UserProfile | null {
+  const stringProfile = localStorage.getItem('profile');
+  if (stringProfile) {
+    const profile = JSON.parse(stringProfile);
+    return profile as UserProfile;
+  }
+  return null;
 }

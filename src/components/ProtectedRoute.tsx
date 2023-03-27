c9e237df7847253824b8886e099098d25f43bb0f
Adriel <adriel.fsantos@outlook.com>
2023-03-27 18:58:00 -0300

    Atualiza implementacao de ProtectedRoute

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -4,11 +4,20 @@ import { selectUser } from '../features/userSlice';
 
 interface IProps {
   children: any;
+  mustBeAuthor?: boolean;
+  mustBeAdmin?: boolean;
 }
 
-const ProtectedRoute = ({ children }: IProps) => {
+const ProtectedRoute = ({
+  children,
+  mustBeAdmin = false,
+  mustBeAuthor = false,
+}: IProps) => {
   const { profile } = useSelector(selectUser);
   if (!profile) return <Navigate to='/login' replace />;
+  if (mustBeAuthor && !profile.isAuthor)
+    return <Navigate to='/login' replace />;
+  if (mustBeAdmin && !profile.isAdmin) return <Navigate to='/login' replace />;
   return children;
 };
 

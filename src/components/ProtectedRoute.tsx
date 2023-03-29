e2ba92ccf4192307c4c367164edd904c93058a5c
Adriel <adriel.fsantos@outlook.com>
2023-03-28 23:53:02 -0300

    Atualiza componente ProtectedRoute

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -6,19 +6,29 @@ interface IProps {
   children: any;
   mustBeAuthor?: boolean;
   mustBeAdmin?: boolean;
+  mustBeAdminOrAuthor?: boolean;
+  mustBeAdminAndAuthor?: boolean;
 }
 
 const ProtectedRoute = ({
   children,
   mustBeAdmin = false,
   mustBeAuthor = false,
+  mustBeAdminOrAuthor = false,
+  mustBeAdminAndAuthor = false,
 }: IProps) => {
   const { profile } = useSelector(selectUser);
+
   if (!profile) return <Navigate to='/login' replace />;
-  if (mustBeAuthor && !profile.isAuthor)
-    return <Navigate to='/login' replace />;
-  if (mustBeAdmin && !profile.isAdmin) return <Navigate to='/login' replace />;
-  return children;
+  if (
+    (mustBeAuthor && profile.isAuthor) ||
+    (mustBeAdmin && profile.isAdmin) ||
+    (mustBeAdminAndAuthor && profile.isAdmin && profile.isAuthor) ||
+    (mustBeAdminOrAuthor && (profile.isAuthor || profile.isAdmin))
+  ) {
+    return children;
+  }
+  return <Navigate to='/login' replace />;
 };
 
 export default ProtectedRoute;
